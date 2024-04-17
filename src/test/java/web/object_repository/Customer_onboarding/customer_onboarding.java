8af4b52a016e032928671824d5e3ca6f154bec8b
Harshal2120 <harshal@qable.io>
2024-04-17 12:00:17 +0530

    added customer onboarding login scenario

--- src/test/java/web/object_repository/Customer_onboarding/customer_onboarding.java
+++ src/test/java/web/object_repository/Customer_onboarding/customer_onboarding.java
@@ -19,5 +19,28 @@ public class customer_onboarding {
         wait = new Wait(driver);
     }
 
+    public void enter_ec_number(String EC_Number) {
+        verify.check_element_is_present("admin_page_ecnumber_text_box");
+        element.click("admin_page_ecnumber_text_box");
+        wait.wait_for_second(2);
+        element.enter_text("admin_page_ecnumber_text_box", EC_Number);
+    }
 
+    public void enter_smart_card_number(String smart_card_number) {
+        verify.check_element_is_present("admin_page_scan_smart_card_text_box");
+        element.click("admin_page_scan_smart_card_text_box");
+        element.enter_text("admin_page_scan_smart_card_text_box", smart_card_number);
+    }
+
+    public void enter_otp(String otp) {
+        verify.check_element_is_present("admin_page_otp_text_box");
+        element.click("admin_page_otp_text_box");
+        element.enter_text("admin_page_otp_text_box", otp);
+    }
+
+    public void click_on_submit_button() {
+        verify.check_element_is_present("admin_page_submit_button");
+        wait.wait_for_second(2);
+        element.click("admin_page_submit_button");
+    }
 }
